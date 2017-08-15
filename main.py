0bfa6078ad943afddfed59ca00529ebe6cf9da7d
Mrinal Pande <me.mrinal@yahoo.com>
2017-08-15 13:03:52 +0530

    Added Readme
    Wrote readme.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-### Title:  Music Based Encryption
 ### Author: Mrinal Pande
 ### Date:   Aug,15 2017
 
 def main():
+    fr = open("./files/Plain_text.txt",'r')
+    plain_text = fr.read()
     
+main()
