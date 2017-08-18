eb7c579c842f685459be1c4deb9b1c9bf4c2cb4d
Mrinal Pande <me.mrinal@yahoo.com>
2017-08-18 16:09:53 +0530

    Fixed
    Fixed the last digit extraction problem

--- encrypt.py
+++ encrypt.py
@@ -2,19 +2,14 @@ def encrypt(path,key,outfile):
     print("\n-------Encrypting-------")
     plain_text = open(str(path),"r")
     for lines in plain_text:
-        pt = lines
+        pt = lines.upper()
+    print("\nChecking Scale...")
     scale = open("./scales/"+key,"r")
     enc = open("./encode/"+key+"_en","r")
     for notes in scale:
         note = notes
-    print(pt)
-    print(note+"\n")
-
-    #variable only consists only 9(the last entry)
-    #variable not getting initialized
-    #check not working
+    print("Plain Text: "+pt)
+    print("Notes on scale: "+note)
     en = []
-    i=0
     for lines in enc:
-        en[i] = lines
-        i = i+1
+        en.append(lines.rstrip('\n'))
