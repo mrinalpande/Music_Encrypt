7c599e0066a6057be75f6a4dbce3eb3c006cd2ac
Mrinal Pande <me.mrinal@yahoo.com>
2017-08-18 15:49:37 +0530

    Encryption algo
    The brain behind encryption.

--- encrypt.py
+++ encrypt.py
@@ -1,4 +1,20 @@
-def encrypt():
-    print("Encrypting...")
-    fo = open("p_text.txt","r")
-    print(fo)
+def encrypt(path,key,outfile):
+    print("\n-------Encrypting-------")
+    plain_text = open(str(path),"r")
+    for lines in plain_text:
+        pt = lines
+    scale = open("./scales/"+key,"r")
+    enc = open("./encode/"+key+"_en","r")
+    for notes in scale:
+        note = notes
+    print(pt)
+    print(note+"\n")
+
+    #variable only consists only 9(the last entry)
+    #variable not getting initialized
+    #check not working
+    en = []
+    i=0
+    for lines in enc:
+        en[i] = lines
+        i = i+1
