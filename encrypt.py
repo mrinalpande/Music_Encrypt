eb2d5e6d33bb2143c48d2f62694e5062fb712cdf
Mrinal Pande <me.mrinal@yahoo.com>
2017-08-21 16:21:26 +0530

    Restructuring
    Restructuring files to makes lists in python easily.

--- encrypt.py
+++ encrypt.py
@@ -7,11 +7,33 @@ def encrypt(path,key,outfile):
         pt = lines.upper()
     print("\nChecking Scale...")
     scale = open("./scales/"+key,"r")
-    enc = open("./encode/"+key+"_en","r")
+    encoding = open("./encode/"+key+"_en","r")
+    duration = open("./encode/duration","r")
+
+    #Creating list for the notes based on the key
+    note = []
     for notes in scale:
-        note = notes
+        note.append(notes.rstrip('\n'))
+
+    #Creating list for the encoding
     en = []
-    for lines in enc:
-        en.append(lines.rstrip('\n'))
-    print(note)
-    generate.generate(note,outfile)
+    for lines in encoding:
+        en.append(lines.rstrip())
+
+    #Creating list for the note duration
+    dur = []
+    for dura in duration:
+        dur.append(dura.rstrip('\n'))
+
+    #Checking
+    print(note[2])
+    print(en)
+    print(dur[4])
+
+
+    #generate.generate(note,dur,outfile)
+    #check encoding
+    # for word in pt:
+    #     if word in en:
+    #         ind = en.index(word)
+    #         print(ind)
--- generate.py
+++ generate.py
@@ -1,9 +1,9 @@
 from pyknon.genmidi import Midi
 from pyknon.music import NoteSeq
 
-def generate(notes,outfile):
+def generate(note,dur,outfile):
     print("Generating music file...")
-    notes1 = NoteSeq("A3 B3 C# D E F# G#")
+    notes1 = NoteSeq(note)
     midi = Midi(1, tempo=90)
     midi.seq_notes(notes1, track=0)
     midi.write(outfile+".mid")
