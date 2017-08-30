a2a3fcc1743254512f648ffeb062522f4e3d1f27
Mrinal Pande <me.mrinal@yahoo.com>
2017-08-30 10:27:03 +0530

    Generation
    Moving to generation part

--- encrypt.py
+++ encrypt.py
@@ -1,23 +1,26 @@
 import generate
-import time
+# import time
 
 def encrypt(path,key,outfile):
     #timing the loops
-    program_starts = time.time()
+    # program_starts = time.time()
+    
     print("\n-------Encrypting-------")
     plain_text = open(str(path),"r")
-    scale = open("./scales/"+key,"r")
+    # scale = open("./scales/"+key,"r")
     encoding = open("./encode/"+key+"_en","r")
     duration = open("./encode/duration","r")
     cipher_text = open("c_text","w")
+    cipher_dur = open("c_dur","w")
     #Getting plain text
     for lines in plain_text:
         pt = lines.upper()
-
+    plain_text.close()
+    
     #Creating list for the notes based on the key
-    note = []
-    for notes in scale:
-        note.append(notes.rstrip('\n'))
+    # note = []
+    # for notes in scale:
+    #     note.append(notes.rstrip('\n'))
 
     #Creating list for the encoding
     en = []
@@ -35,11 +38,15 @@ def encrypt(path,key,outfile):
             cipher_text.write(str(letters.find(let)).rstrip('-1'))
             for letter in letters:
                 if letter == let:
-                    cipher_text.write(","+dur[en.index(letters)]+"\n")
+                    cipher_dur.write(dur[en.index(letters)]+"\n")
                     break
                 else:
                     continue
-    now = time.time()
-    taken = now - program_starts
-    print("Time taken",taken)
-    #generate.generate(note,dur,outfile)
+    cipher_text.close()
+    cipher_dur.close()
+    duration.close()
+    encoding.close()
+    # now = time.time()
+    # taken = now - program_starts
+    # print("Time taken",taken)
+    generate.generate(key,outfile)
